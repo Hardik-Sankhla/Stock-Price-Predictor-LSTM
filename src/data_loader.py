b71490328b7c5ae44c6410affa8e5c87dd1258ce
Hardik Sankhla <datascientist.hardiksankla@gmail.com>
2025-04-24 02:06:00 +0530

    Updated data loader and added new models and artifacts

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,8 +1,17 @@
 # src/data_loader.py
 import yfinance as yf
 import pandas as pd
+import os
 
 def download_data(ticker="AAPL", start="2015-01-01", end="2021-01-01"):
+    # Check if the 'data' directory exists, if not, create it
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+    # Download stock data using yfinance
     df = yf.download(ticker, start=start, end=end)
-    df.to_csv("data/raw_stock.csv")
+
+    # Save the data as CSV in the 'data' folder
+    df.to_csv(f"data/raw_stock_{ticker}.csv")
+
     return df
